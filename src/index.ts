4504956f8f8c4360a562b6c673deabb17524f7d6
KoLiBer <koliberr136a1@gmail.com>
2019-08-13 13:42:27 +0430

    fix: add imports to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from "./component";
 export * from "./types";
 export * from "./keys";
+
+export * from "./models";
+export * from "./repositories";
+export * from "./decorators";
+export * from "./repositories";
