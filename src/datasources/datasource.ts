823eb36dfd7eef15ad5df22667df0266c18dd8ad
KoLiBer <koliberr136a1@gmail.com>
2019-12-06 21:25:02 +0330

    fix: add datasource constructor

--- src/datasources/datasource.ts
+++ src/datasources/datasource.ts
@@ -1,6 +1,11 @@
 import { juggler } from "@loopback/repository";
+import { Options, ModelBuilder } from "loopback-datasource-juggler";
 
 import { bindAuthorization } from "~/keys";
 
 @bindAuthorization("DataSource")
-export class DataSource extends juggler.DataSource {}
+export class DataSource extends juggler.DataSource {
+    constructor(settings?: Options, modelBuilder?: ModelBuilder) {
+        super(settings, modelBuilder);
+    }
+}
