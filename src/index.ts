1a03dff562c9607d6535d87ec2dd65b64a0bd0c5
KoLiBer <koliberr136a1@gmail.com>
2019-12-06 23:39:24 +0330

    fix: remove datasources

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from "./keys";
 export * from "./types";
 
 export * from "./models";
-export * from "./datasources";
 export * from "./repositories";
 export * from "./decorators";
 export * from "./mixins";
