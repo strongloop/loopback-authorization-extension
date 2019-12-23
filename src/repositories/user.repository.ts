3340e427a0f680b89f41b4ae1faa9316d5aa65ec
KoLiBer <koliberr136a1@gmail.com>
2019-12-23 20:49:21 +0330

    feat: add hasMany relations to repositories

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,22 +1,40 @@
-import { inject } from "@loopback/context";
-import { juggler } from "@loopback/repository";
+import { inject, Getter } from "@loopback/context";
+import { juggler, HasManyRepositoryFactory } from "@loopback/repository";
 import { Ctor, HistoryCrudRepository } from "loopback-history-extension";
 
-import { bindAuthorization, PrivateAuthorizationBindings } from "../keys";
+import {
+    bindAuthorization,
+    AuthorizationBindings,
+    PrivateAuthorizationBindings
+} from "../keys";
 
-import { User, UserRelations } from "../models";
+import { User, UserRelations, UserRole } from "../models";
+
+import { UserRoleRepository } from "./";
 
 @bindAuthorization("UserRepository")
 export class UserRepository<
     Model extends User,
     ModelRelations extends UserRelations
 > extends HistoryCrudRepository<Model, ModelRelations> {
+    public readonly userRoles: HasManyRepositoryFactory<
+        UserRole,
+        typeof User.prototype.id
+    >;
+
     constructor(
         @inject(PrivateAuthorizationBindings.USER_MODEL)
         ctor: Ctor<Model>,
         @inject(PrivateAuthorizationBindings.DATASOURCE)
-        dataSource: juggler.DataSource
+        dataSource: juggler.DataSource,
+        @inject.getter(AuthorizationBindings.USER_ROLE_REPOSITORY)
+        getUserRoleRepository: Getter<UserRoleRepository>
     ) {
         super(ctor, dataSource);
+
+        this.userRoles = this.createHasManyRepositoryFactoryFor(
+            "userRoles",
+            getUserRoleRepository
+        );
     }
 }
